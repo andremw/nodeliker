e434f62dc298339faf6580dfe7113c153cfe18c9
unknown <andremw@CIT0010121BH.cit>
2014-11-04 15:01:15 -0200

    Notifications to alert whenever the token expires

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 'use strict';
 
 var graph = require('fbgraph'),
+    notifier = require('node-notifier'),
     likedPosts = [],
     lastPost = null,
-    _accessToken = 'my_token';
+    _accessToken = 'CAACEdEose0cBAO0MEhgzMcaFwvooIu3rjCWymjXPZAZCznP92wY328VQyAlbeGO7QBliMXpl7BfazhATvCLOjgY2dBGUSm4AWQPbzAAk0atABm7TpE2h1RpbbpDMqCtzymJ4MNzDQqvcxveQ5Y9ZBZBixip3blZA9YzXWz0zYU70ElMEp5y5ZBzI5SLZBNptFnXpz42ZAZAnyNl8zYUdxuVnh';
 
 graph.setAccessToken(_accessToken);
 
@@ -20,6 +21,14 @@ setInterval(function() {
   console.log('Checking for new page posts..\n');
 
   graph.get('/119269378147423/feed', function (err, res) {
+    if (err) {
+      notifier.notify({
+        title: 'Error!',
+        message: 'Something went wrong: ' + err.message + '\nType: ' + err.type,
+        time: 50000,
+        wait: true
+      });
+    }
     if (res && res.data) {
       lastPost = res.data[0];
       if (likedPosts.indexOf(lastPost.id) < 0)
